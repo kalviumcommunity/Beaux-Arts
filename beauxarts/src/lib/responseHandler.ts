ee46b08f825011e79451e0e1315c52c9c3998408
AbirpandA <abirpanda335@gmail.com>
2025-12-25 21:11:41 +0530

    feat: Added general api response Handler

---
+++ beauxarts/src/lib/responseHandler.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+
+// Standardize the response structure type
+type ApiResponse<T = null> = {
+  success: boolean;
+  message: string;
+  data?: T;
+  error?: { code: string; details?: unknown };
+  timestamp: string;
+};
+
+
+export const sendSuccess = <T>({
+  data,
+  message = "Success",
+  status = 200,
+}: {
+  data: T;
+  message?: string;
+  status?: number;
+}) => {
+  return NextResponse.json<ApiResponse<T>>(
+    {
+      success: true,
+      message,
+      data,
+      timestamp: new Date().toISOString(),
+    },
+    { status }
+  );
+};
+
+export const sendError = ({
+  message = "Something went wrong",
+  code = "INTERNAL_ERROR",
+  status = 500,
+  details = null,
+}: {
+  message?: string;
+  code?: string;
+  status?: number;
+  details?: unknown; 
+} = {}) => {
+  return NextResponse.json<ApiResponse>(
+    {
+      success: false,
+      message,
+      error: { code, details },
+      timestamp: new Date().toISOString(),
+    },
+    { status }
+  );
+};
